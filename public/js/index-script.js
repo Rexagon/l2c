75fb24eabae293a0d2f72a5669787137d5a02036
Rexagon <reide740@gmail.com>
2016-10-31 20:43:46 +0300

    auto commit

--- public/js/index-script.js
+++ public/js/index-script.js
@@ -64,4 +64,96 @@ $(document).ready(function() {
     setInterval(function(){
         phraseBlock.text(phrases[i++ % phrases.length]);
     }, 5000);
+
+
+    var dialogs = [{
+        description: '<p>Landing Page - посадочная страница или же продающая страница,необходима в том случае,если Вы можете чётко сформулировать: "Что именно Вы хотите от клиента,что он должен сделать?" К примеру, купить утюг, записаться на услугу, оставить Вам свои контакты и т.д</p><br>\
+                  <p>Мы создаём продающую страницу,которая отражает все преимущества Вашего товара или услуги,внушает доверие клиента к Вашему бренду,и главное поэтапно ведёт его к совершению ЦЕЛЕВОГО(главного) действия(покупка,запись на приём,отправление Вам контактов).</p>',
+        list_title: 'Что Вы получите после создания Landing Page?',
+        list: [
+            'Увеличения продаж/заказов какой-либо конкретной услуги.', 
+            'Получение контактных данных клиента.(используется для рекламы) ',
+            'Возможность рассказать большой аудитории о создание нового направления в бизнесе.(продажа нового товара)',
+            'Возможность адаптации описания товара для разной аудитории.(Например,если человек ищет "Купить шины в Москве",то именно эту фразу он увидит на посадочной странице и т.д)',
+            'Возможность возвращать клиентов,которые хотят уйти с Вашего основного сайта,путём перенаправления их на Landing Page,к примеру,с предоставлением скидки)'
+        ],
+        addition: 'qqweewe',
+        question: 'Как поможет landing page вашему бизнесу?'
+    }, {
+        description: 'asd',
+        list_title: '',
+        list: [],
+        addition: '',
+        question: ''
+    }, {
+        description: 'q',
+        list_title: '',
+        list: [],
+        addition: '',
+        question: ''
+    }, {
+        description: 'w',
+        list_title: '',
+        list: [],
+        addition: '',
+        question: ''
+    }, {
+        description: 'e',
+        list_title: '',
+        list: [],
+        addition: '',
+        question: ''
+    }, {
+        description: 'r',
+        list_title: '',
+        list: [],
+        addition: '',
+        question: ''
+    }, {
+        description: 't',
+        list_title: '',
+        list: [],
+        addition: '',
+        question: ''
+    }, {
+        description: 'y',
+        list_title: '',
+        list: [],
+        addition: '',
+        question: ''
+    }];
+
+    var modal = $('#modal');
+    $('.price-block').click(function() {
+        var id = $('.price-block').index($(this));
+        var dialog =  dialogs[id];
+        $('#modal-title').text($(this).find('h3')[0].innerHTML);
+
+        var image = $($(this).find('.img')[0].cloneNode(true));
+        image.css({
+            width: '100%'
+        });
+        $('#modal-image').html(image);
+        $('#modal-description').html(dialog.description);
+        $('#modal-list-title').html(dialog.list_title);
+
+        var list = '';
+        $.each(dialog.list, function(index, value) {
+            list += '<li>' + value + '</li>';
+        });
+        $('#modal-list').html(list);
+        $('#modal-addition').html(dialog.addition);
+        $('#modal-question').html('<b>' + dialog.question + '</b><br><small>Наши специалисты подготовят для Вашей сферы бизнеса персональное предложение, которое заинтересует именно ваших клиентов.</small>');
+        modal.show();
+    });
+
+    $('#modal-close').click(function() {
+        modal.hide();
+    });
+
+    $(window).click(function(event) {
+        if (event.target.id == "modal") {
+            modal.hide();
+        }
+    });
 });
